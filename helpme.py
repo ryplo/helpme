be6a7e83ea7d6cffc08ffba9ab5d658ef988f5f1
ryplo <lo.rachel8@gmail.com>
2016-09-17 15:21:09 -0400

    WIP - tries to add beeping sound

--- helpme.py
+++ helpme.py
@@ -1,14 +1,25 @@
 import pypd
+import os
 pypd.api_key = "xx3gyxWCgxhQtvxs1K6B"
-pypd.Event.create(data={
-    'service_key': '266cc74b1bef4ad8be210d0819a436f8',
-    'event_type': 'trigger',
-    'description': 'this is a trigger event!',
-    'contexts': [
-          {
-              'type': 'link',
-              'href': 'http://bushdid7111.pagerduty.com',
-              'text': 'View on PD',
-          },
-    ],
-})
+# find incidents, resolve the unresolvd ones.
+
+triggered = False
+if True: # if Sensor.fall == True
+    pypd.Event.create(data={
+        'service_key': '266cc74b1bef4ad8be210d0819a436f8',
+        'event_type': 'trigger',
+        'description': 'this is a trigger event!',
+        'contexts': [
+              {
+                  'type': 'link',
+                  'href': 'http://bushdid7111.pagerduty.com',
+                  'text': 'View on PD',
+              },
+        ],
+    })
+    triggered = True
+while triggered == True:
+    file = ' /Users/rachello/dev/helpme/beep-05.wav'
+    os.system ('mplayer' + file)
+    if True:
+        triggered = False
