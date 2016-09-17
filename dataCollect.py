6a928dfc09249dedaf51223ea18642f1f6a11b58
Himel Mondal <himel131@gmail.com>
2016-09-17 20:32:10 +0000

    9 readings + wireless rpi readings

--- dataCollect.py
+++ dataCollect.py
@@ -30,22 +30,24 @@ imu.setGyroEnable(True)
 imu.setAccelEnable(True)
 imu.setCompassEnable(True)
 
-f = open('compass.csv',"w")
+f = open('Walking.csv',"w")
 
 poll_interval = imu.IMUGetPollInterval()
 print("Recommended Poll Interval: %dmS\n" % poll_interval)
 count = 0
 initTime= time.time()
 print(initTime)
-while (time.time()-initTime<20): 
+while (time.time()-initTime<10): 
   if imu.IMURead():
     # x, y, z = imu.getFusionData()
     # print("%f %f %f" % (x,y,z))
     data = imu.getIMUData()
-    fusionPose = data["accel"]
+    accel = data["accel"]
+    gyr = data["gyro"]
+    compass = data["compass"]
 ##    print("r: %f p: %f y: %f" % (fusionPose[0], 
 ##        fusionPose[1], fusionPose[2]))
-    f.write(str(fusionPose[0])+","+str(fusionPose[1])+","+str(fusionPose[2])+"\n")
+    f.write(str(accel[0])+","+str(accel[1])+","+str(accel[2])+","+str(gyr[0])+","+str(gyr[1])+","+str(gyr[2])+","+str(compass[0])+","+str(compass[1])+","+str(compass[2])+"\n")
     count+=1
     time.sleep(poll_interval*1.0/1000.0)
 ##print(time.time()-initTime)
